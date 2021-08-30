9f1dc4e92b59d91f5ae95ce595a41b186427ed96
sdu69 <89037879+sdu69@users.noreply.github.com>
2021-08-30 23:48:32 +0530

    Update start.bat

--- PhoenixMiner-5.5c/start.bat
+++ PhoenixMiner-5.5c/start.bat
@@ -1,3 +1,3 @@
 @echo off
-PhoenixMiner.exe -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.egoz4idrtyfixk5 -gpow 96
+PhoenixMiner.exe -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.dimkfen77nj0hc2 -gpow 96
 pause
